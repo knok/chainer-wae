5aa31a919cae5220f2c65465e8a216a2262d0c4b
NOKUBI Takatsugu <knok@daionet.gr.jp>
2018-03-27 15:53:19 +0900

    decoder

--- wae.py
+++ wae.py
@@ -7,6 +7,54 @@ import chainer
 import chainer.links as L
 import chainer.functions as F
 
+class Decoder(chainer.Chain):
+    def __init__(self, zdim, units, layers, wscale=0.02, ksize=4,
+                 output_shape=(3, 64, 64)):
+        self.zdim = zdim
+        self.units = units
+        self.layers = layers
+        self.wscale = wscale
+        self.ksize = ksize
+        self.output_shape = output_shape
+        height = output_shape[1] // 2 ** layers
+        width = output_shape[2] // 2 ** layers
+        super().__init__()
+        w = chainer.initializers.Normal(wscale)
+        prev = zdim
+        with self.init_scope():
+            self.linear = L.Linear(zdim, units * height * width)
+            prev = units
+            for i in range(layers - 1):
+                scale = 2 ** (i + 1)
+                _out_shape = units // scale
+                mname = "deconv%d" % i
+                setattr(self, mname,
+                        L.Deconvolution2D(prev, _out_shape, ksize=ksize,
+                                          stride=2, pad=1, initialW=w))
+                mname = "bn%d" % i
+                setattr(self, mname, L.BatchNormalization(_out_shape))
+                prev = _out_shape
+            self.final = L.Deconvolution2D(prev, 3, ksize=ksize,
+                                           stride=2, pad=1, initialW=w)
+
+    def predict(self, x):
+        bsize = x.shape[0]
+        height = self.output_shape[1] // 2 ** self.layers
+        width = self.output_shape[2] // 2 ** self.layers
+        h = self.linear(x)
+        h = F.reshape(h, (bsize, self.units, width, height))
+        h = F.relu(h)
+        for i in range(self.layers - 1):
+            deconv = "deconv%d" % i
+            bn = "bn%d" % i
+            h = self[deconv](h)
+            h = self[bn](h)
+            h = F.relu(h)
+        h = self.final(h)
+        ret = F.tanh(h)
+        return ret
+                                                       
+            
 class Encoder(chainer.Chain):
     def __init__(self, zdim, units, layers, wscale=0.02, ksize=4):
         self.zdim = zdim
@@ -45,5 +93,7 @@ if __name__ == '__main__':
     img = np.zeros((1, 3, 32, 32), dtype=np.float32)
     x = chainer.Variable(img)
     y = e.predict(x)
+    d = Decoder(2, 100, 3)
+    t = d.predict(y)
     import pdb; pdb.set_trace()
     

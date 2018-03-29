9b0ac9ca6770f54eeb1f1fa63c4573fa7f97715a
NOKUBI Takatsugu <knok@daionet.gr.jp>
2018-03-29 11:59:23 +0900

    plot centered images

--- plot.py
+++ plot.py
@@ -6,6 +6,7 @@ import argparse
 import numpy as np
 import chainer
 import matplotlib.pyplot as plt
+import matplotlib
 
 import wae
 import data
@@ -34,21 +35,52 @@ def main():
 
     data_iter = chainer.iterators.SerialIterator(dataset, args.batchsize,
                                                  repeat=False, shuffle=False)
+    imgs = []
     vectors = []
     with chainer.using_config('train', False):
         for d in data_iter:
             d = np.array(d)
+            imgs.append(d)
             x = chainer.Variable(d)
             sample_vec = model.enc(d)
             if args.gpu >=0:
                 sample_vec = chainer.cuda.to_cpu(sample_vec)
             vectors.append(sample_vec.data)
+
+    imgs = np.vstack(imgs).transpose(0, 2, 3, 1)
+    imgs = imgs * 0.5 + 0.5
     z = np.vstack(vectors)
+
+    zz = []
+    z_idx = []
+    for i in range(z.shape[0]):
+        t = 0.5
+        if z[i, 0] < t and z[i, 0] > -t and z[i, 1] < t and z[i, 1] > -t:
+            zz.append(z[i])
+            z_idx.append(i)
+    zz = np.array(zz)
+    z_imgs = imgs[z_idx]
+    l = z_imgs.shape[0]
+    cols = 8
+    m = (cols - l % cols)
+    filler = np.ones((m, z_imgs.shape[1], z_imgs.shape[2], z_imgs.shape[3]),
+                     dtype=np.float32)
+    z_imgs = np.vstack([z_imgs, filler])
+    zi = np.concatenate(np.split(z_imgs, cols), axis=2)
+    zi = np.concatenate(zi, axis=0)
+    #import pdb; pdb.set_trace()
+
+    gs = matplotlib.gridspec.GridSpec(2, 1)
+    ax = plt.subplot(gs[0, 0])
+    plt.scatter(z[:, 0], z[:, 1])
+    plt.scatter(zz[:, 0], zz[:, 1], color="red")
+    ax = plt.subplot(gs[1, 0])
+    plt.imshow(zi)
+    plt.show()
+    plt.close()
+    
     # import pdb; pdb.set_trace()
     # pass
-
-    plt.scatter(z[:, 0], z[:, 1])
-    plt.show()
 
 if __name__ == '__main__':
     main()
